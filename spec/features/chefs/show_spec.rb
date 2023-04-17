29c47cc236853a51d072473e72d365894557b973
margdaly <margaret.daly@gmail.com>
2023-04-16 22:00:17 -0600

    Add chef show page

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Chef Show Page' do
+  describe 'As a visitor' do
+    before :each do
+      @chef1 = Chef.create!(name: "Chef1")
+      @chef2 = Chef.create!(name: "Chef2")
+      @salad = @chef1.dishes.create!(name: "Salad", description: "Leafy Greens")
+      @donut = @chef2.dishes.create!(name: "Donut", description: "The Best")
+      @frosting = @chef2.dishes.create!(name: "Frosting", description: "The Absolute Best")
+      @lettuce = Ingredient.create!(name: "Lettuce", calories: 100)
+      @croutons = Ingredient.create!(name: "Croutons", calories: 200)
+      @flour = Ingredient.create!(name: "Flour", calories: 800)
+      @sugar = Ingredient.create!(name: "Sugar", calories: 900)
+      @butter = Ingredient.create!(name: "Butter", calories: 700)
+      DishIngredient.create!(dish: @salad, ingredient: @lettuce)
+      DishIngredient.create!(dish: @salad, ingredient: @croutons)
+      DishIngredient.create!(dish: @donut, ingredient: @flour)
+      DishIngredient.create!(dish: @donut, ingredient: @sugar)
+      DishIngredient.create!(dish: @frosting, ingredient: @butter)
+      DishIngredient.create!(dish: @frosting, ingredient: @sugar)
+    end
+
+    it 'I see a link to view a list of all ingredients that this chef uses in their dishes.' do
+      visit chef_path(@chef2)
+      expect(page).to have_link("View All Ingredients")
+      visit chef_path(@chef1)
+      expect(page).to have_link("View All Ingredients")
+    end
+
+    it "When I click on that link
+    I'm taken to a chef's ingredients index page
+    and I can see a unique list of names of all the ingredients that this chef uses" do
+      
+    end
+  end
+end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -6,5 +6,27 @@ RSpec.describe Chef, type: :model do
   end
   describe "relationships" do
     it {should have_many :dishes}
+    it {should have_many(:ingredients).through(:dishes)}
+  end
+
+  describe 'instance methods' do
+    before :each do
+      @chef2 = Chef.create!(name: "Chef2")
+      @donut = @chef2.dishes.create!(name: "Donut", description: "The Best")
+      @frosting = @chef2.dishes.create!(name: "Frosting", description: "The Absolute Best")
+      @flour = Ingredient.create!(name: "Flour", calories: 800)
+      @sugar = Ingredient.create!(name: "Sugar", calories: 900)
+      @butter = Ingredient.create!(name: "Butter", calories: 700)
+      DishIngredient.create!(dish: @donut, ingredient: @flour)
+      DishIngredient.create!(dish: @donut, ingredient: @sugar)
+      DishIngredient.create!(dish: @frosting, ingredient: @butter)
+      DishIngredient.create!(dish: @frosting, ingredient: @sugar)
+    end
+
+    describe '.unique_ingredients' do
+      it 'returns a unique list of ingredients' do
+        expect(@chef2.unique_ingredients).to eq([@flour, @sugar, @butter])
+      end
+    end
   end
 end
